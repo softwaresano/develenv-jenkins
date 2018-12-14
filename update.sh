e5b64c25183523b2fa40c3ba9dd5c9348c9266c3
carlosg <carlosenrique.gomezgomez@telefonica.com>
2018-12-14 10:54:47 +0100

    update.sh fixed

--- update.sh
+++ update.sh
@@ -5,13 +5,13 @@ jenkins_artifacts=/var/develenv/repositories/artifacts/develenv/jenkins
 jenkins_output_dir=${jenkins_artifacts}/home/develenv/app/jenkins/plugins/
 jenkins_war_dir=${jenkins_artifacts}/usr/lib/jenkins
 mkdir -p $jenkins_output_dir $jenkins_war_dir
-curl -L -k http://mirrors.jenkins.io/war/latest/jenkins.war >$jenkins_war_dir/jenkins.war
+curl -f -L -k http://mirrors.jenkins.io/war/latest/jenkins.war >$jenkins_war_dir/jenkins.war || exit 1
 
 rm -Rf $jenkins_output_dir/*.jpi
 for i in $(ls $plugins_dir/*.jpi); do 
   plugin=$(basename $i)
   plugin_name=${plugin/.jpi/}
   echo $plugin_name
-  echo curl -L -k ${jenkins_plugins_url}/${plugin/.jpi/.hpi} 
-  curl -L -k ${jenkins_plugins_url}/${plugin/.jpi/.hpi} > $jenkins_output_dir/$plugin
+  echo curl -f -L -k ${jenkins_plugins_url}/${plugin/.jpi/.hpi} 
+  curl -f -L -k ${jenkins_plugins_url}/${plugin/.jpi/.hpi} > $jenkins_output_dir/$plugin || exit 1
 done;
